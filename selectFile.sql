aca2ad35075c67599240f37280b01deebbd90d66
lirralirra <2312168096@qq.com>
2024-11-13 21:04:00 +1030

    updated

--- selectFile.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM crowdfunding_schema.categories;
-SELECT * FROM crowdfunding_schema.subcategories;
-SELECT * FROM crowdfunding_schema.contacts;
-SELECT * FROM crowdfunding_schema.campaign;
