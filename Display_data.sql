994647fab0a2efa0b93bcafd152fa15dd34e94a4
lirralirra <2312168096@qq.com>
2024-11-14 20:42:49 +1030

    final version, files updates

---
+++ Display_data.sql
@@ -0,0 +1,11 @@
+-- Retrieve all data from the Campaign table
+SELECT * FROM "campaign";
+
+-- Retrieve all data from the Categories table
+SELECT * FROM "category";
+
+-- Retrieve all data from the Contacts table
+SELECT * FROM "contacts";
+
+-- Retrieve all data from the Subcategories table
+SELECT * FROM "subcategory";
